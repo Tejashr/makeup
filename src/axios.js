a58496deec4d9755c8654b494de74b236a62fd68
Tejashr <tejasteju11@gmail.com>
2021-07-16 21:42:45 +0530

    added comments

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+//getting the details of the product using given json data
 export function GetDetails(){
     return axios.get(`http://makeup-api.herokuapp.com/api/v1/products.json`)
 }
