3480bd044bda6474d76bdd84f683c563919a7cb1
Tejashr <tejasteju11@gmail.com>
2021-07-16 21:41:12 +0530

    added comments

--- src/cart.js
+++ src/cart.js
@@ -22,15 +22,15 @@ function Cart() {
                                 <div class="card" style={{ width: "18rem" }}>
                                     <img class="card-img-top" style={{ height: "18rem" }} src={`${user.image_link}`} alt="Image not found" />
                                     <div class="card-body bg-warning">
-                                        <h5 class="card-title">{user.name}</h5>
-                                        <p class="card-text">{user.brand}</p>
+                                        <h5 class="card-title">{user.name}</h5>{/* displaying name of the product */}
+                                        <p class="card-text">{user.brand}</p>{/* displaying brand of the product */}
                                         <h6 className="card-text">Price: Rs{user.price}</h6>
                                     </div>
                                     <ul class="list-group list-group-flush">
-                                        <li class="list-group-item">{user.description}</li>
+                                        <li class="list-group-item">{user.description}</li>{/* displaying description of the product */}
                                     </ul>
                                     <div class="card-footer bg-warning">
-                                        <a href={`${user.product_link}`} target="_blank" class="card-link text-dark">Product link</a>
+                                        <a href={`${user.product_link}`} target="_blank" class="card-link text-dark">Product link</a>{/* redirecting to the product link to new page */}
                                     </div>
                                 </div>
                             </div>
